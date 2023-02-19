8d99d1c7434a1f98495f97235c31ba7df8df628d
Vicky <vicky9060993097@gmail.com>
2023-02-19 10:20:27 +0530

    Initial Upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1> <i className="fa-regular fa-note-sticky"></i>  Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
